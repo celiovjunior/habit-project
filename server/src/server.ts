535ccc33d467eefc6f1451e850e5cc848c5f39e7
Célio V. Júnior <celio_cjr@hotmail.com>
2023-01-16 21:03:44 -0300

    dev: config prisma, made the first model and tested

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
-import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client'
+import Fastify from 'fastify'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return 'hello oie!'
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()  
+  return habits
 })
 
 app.listen({
