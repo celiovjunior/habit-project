9fc50da8e6323ded69f88545b716853218dd4bcd
Célio V. Júnior <celio_cjr@hotmail.com>
2023-01-19 17:18:28 -0300

    dev: built the toggle habit route and made a few tests

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -72,5 +72,52 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits
     }
   })
+
+  // complete / not-complete a habit
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = toggleHabitParams.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    var day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      }
+    })
+
+    if (dayHabit) {
+      // remove completed mark
+    } else {
+      // complete habit
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      })
+    }
+
+  })
 }
 
