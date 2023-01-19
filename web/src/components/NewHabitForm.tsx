6025d3f68cc508e87be1b2834e056bfddb09b4bb
Célio V. Júnior <celio_cjr@hotmail.com>
2023-01-19 19:13:53 -0300

    dev: made the modal, installed radix ui to built dialog content

---
+++ web/src/components/NewHabitForm.tsx
@@ -0,0 +1,28 @@
+import { Check } from "phosphor-react";
+
+export function NewHabitFunction () {
+  return(
+    <form className="w-full flex flex-col mt-6">
+      <label htmlFor="title" className="font-semibold leading-tight">
+        Qual seu comprometimento?
+      </label>
+
+      <input 
+        type="text"
+        id="title"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        placeholder="ex.: Exercícios, dormir bem, etc..." 
+        autoFocus
+      />
+
+      <label htmlFor="" className="font-semibold leading-tight mt-4">
+        Qual a recorrência?
+      </label>
+
+      <button type="submit" className="mt-6 rounded-lg p-4 gap-3 flex items-center font-semibold bg-green-600 justify-center hover:bg-green-500">
+        <Check size={24} weight="bold" />
+        Confirmar
+      </button>
+    </form>
+  )
+}
