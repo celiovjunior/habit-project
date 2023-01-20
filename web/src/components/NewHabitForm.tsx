c9fe707b8ac72ec7acb6ea0a6d02399fd69726d2
Célio V. Júnior <celio_cjr@hotmail.com>
2023-01-20 01:42:53 -0300

    dev: styling the details and checkbox dialog popover and set up the rest of week day at the create habit form

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,4 +1,15 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+
+const avaiableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+]
 
 export function NewHabitFunction () {
   return(
@@ -18,6 +29,42 @@ export function NewHabitFunction () {
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+
+      {
+        avaiableWeekDays.map((weekDay, i) => {
+          return (
+            <Checkbox.Root key={i} className='flex items-center gap-3 group'>
+            <div className='
+              h-8
+              w-8
+              rounded-lg
+              flex 
+              items-center 
+              justify-center 
+              bg-zinc-900 
+              border-2 
+              border-zinc-800 
+              group-data-[state=checked]:bg-green-500 
+              group-data-[state=checked]:border-green-400'>
+    
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+    
+            </div>
+    
+            <span className='text-white font-semibold leading-tight'>
+              {weekDay}
+            </span>
+          </Checkbox.Root>
+          )
+        })
+      }
+
+
+      </div>
 
       <button type="submit" className="mt-6 rounded-lg p-4 gap-3 flex items-center font-semibold bg-green-600 justify-center hover:bg-green-500">
         <Check size={24} weight="bold" />
